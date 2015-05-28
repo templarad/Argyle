415d05880783df0617b661b2022fbc6c861e22b3
vonbolt <guozhongxiao1990@gmail.com>
2015-05-28 10:53:34 +0900

    modify classpath and CodeCloneDetectDataReceiver.

--- jp.ac.kyushu.Argyle.impl/src/jp/ac/kyushu/argyle/impl/model/CodeCloneDetectDataReceiver.java
+++ jp.ac.kyushu.Argyle.impl/src/jp/ac/kyushu/argyle/impl/model/CodeCloneDetectDataReceiver.java
@@ -59,6 +59,17 @@ public class CodeCloneDetectDataReceiver extends DataReceiver{
 	
 	@Override
 	protected void invoke(String[] parameters) {
+//		for(int i=0;i<parameters.length;i++)
+//			System.out.println(parameters[i]);
+		parameters = new String[8];
+		parameters[0] = "-d";
+		parameters[1] = "C:\\Anko\\ArgyleMethodList.java";		
+		parameters[2] = "-o";
+		parameters[3] = "C:\\Anko\\scorpio_test.txt";
+		parameters[4] = "-s";
+		parameters[5] = "1";
+		parameters[6] = "-t";
+		parameters[7] = "2";
 		Scorpio.main(parameters);
 	}
 
